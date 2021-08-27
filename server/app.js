94ae11df21a499d1aaa38bd100938f57107a359a
Sam Hilliard <samhilliard51@gmail.com>
2021-08-26 22:30:53 -0700

    back-bones for mongodb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3001 || process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Hey sexy!");
+mongoose.connect('mongodb://localhost:27017/daily_goals', { useNewUrlParser: true }).
+  catch(error => handleError(error));
+
+// my schema (will be put into separate files later)
+const Schema = mongoose.Schema;
+
+const goalSchema = new Schema({
+    name: String,
+    isComplete: Boolean,
+    streak: Number
 });
+
+const Goal = new mongoose.model('Goal', goalSchema);
+
+// get all the goals from the database
+app.get('/api/goals', (res, req) => {
+
+});
+
+// insert a goal
+app.post('/api/add_goal', (res, req) => {
+
+});
+
+// delete a goal by id
+app.post('/api/delete_goal' (res, req) => {
+
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
